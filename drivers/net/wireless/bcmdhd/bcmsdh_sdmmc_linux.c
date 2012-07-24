e3ff3c3332832fbb30227a2f34205c319cb677d3
Om Prakash Singh <omp@nvidia.com>
2012-07-24 21:57:06 +0530

    net: wireless: bcmdhd: power off card when not in use
    Power off the card when wifi is off and power up only when wifi
    is turned on
    
    Bug 1011349
    
    Change-Id: I26a8188d932516c0490dec858acd9e8ea2c5adf8
    Signed-off-by: Om Prakash Singh <omp@nvidia.com>
    Reviewed-on: http://git-master/r/118097
    Reviewed-by: Pavan Kunapuli <pkunapuli@nvidia.com>
    Reviewed-by: Bharat Nihalani <bnihalani@nvidia.com>
    Tested-by: Bharat Nihalani <bnihalani@nvidia.com>
    Reviewed-by: Laxman Dewangan <ldewangan@nvidia.com>

--- drivers/net/wireless/bcmdhd/bcmsdh_sdmmc_linux.c
+++ drivers/net/wireless/bcmdhd/bcmsdh_sdmmc_linux.c
@@ -34,6 +34,7 @@
 
 #include <linux/mmc/core.h>
 #include <linux/mmc/card.h>
+#include <linux/mmc/host.h>
 #include <linux/mmc/sdio_func.h>
 #include <linux/mmc/sdio_ids.h>
 
@@ -131,6 +132,8 @@ static int bcmsdh_sdmmc_probe(struct sdio_func *func,
 #endif
 		sd_trace(("F2 found, calling bcmsdh_probe_bcmdhd...\n"));
 		ret = bcmsdh_probe_bcmdhd(&func->dev);
+		if (mmc_power_save_host(func->card->host))
+			sd_err(("%s: card power save fail", __FUNCTION__));
 	}
 
 	return ret;
