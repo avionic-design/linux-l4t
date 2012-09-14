a02848dfa9e4b12aba85d63cff3899b8d414bf84
Soeren Grunewald <soeren.grunewald@avionic-design.de>
2012-09-14 10:57:16 +0200

    medcom: Debounce sx8634 sensor pads.
    This will enable and set the button-touch-debounce value to avoid randomly
    self-releasing touch events.
    
    Signed-off-by: Soeren Grunewald <soeren.grunewald@avionic-design.de>
    Reviewed-by: Thierry Reding <thierry.reding@avionic-design.de>

--- arch/arm/mach-tegra/board-medcom.c
+++ arch/arm/mach-tegra/board-medcom.c
@@ -280,6 +280,7 @@ static struct adnp_platform_data medcom_adnp_pdata = {
 #define SX8634_DEFAULT_THRESHOLD	0x45
 
 static struct sx8634_platform_data medcom_keypad1_pdata = {
+	.debounce = 3,
 	.caps = {
 		[1] = {
 			.mode = SX8634_CAP_MODE_BUTTON,
@@ -322,6 +323,7 @@ static struct sx8634_platform_data medcom_keypad1_pdata = {
 
 
 static struct sx8634_platform_data medcom_keypad2_pdata = {
+	.debounce = 3,
 	.caps = {
 		[1] = {
 			.mode = SX8634_CAP_MODE_BUTTON,
