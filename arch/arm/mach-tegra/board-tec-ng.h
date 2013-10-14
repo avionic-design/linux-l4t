a1e0c854cc64e8e1247251a572c3449063ad5e5d
Alban Bedel <alban.bedel@avionic-design.de>
2013-10-14 17:34:18 +0200

    arm: tegra: tec-ng: Fix the license header indentation
    Change-Id: I2ff9b602e5253fbaddbf41196bb44cca453013b5
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/524
    Reviewed-by: Soeren Grunewald <soeren.grunewald@avionic-design.de>

--- arch/arm/mach-tegra/board-tec-ng.h
+++ arch/arm/mach-tegra/board-tec-ng.h
@@ -1,21 +1,18 @@
 /*
  * arch/arm/mach-tegra/board-tec-ng.h
  *
- * Copyright (c) 2013, Avionic Design GmbH
- * Copyright (c) 2013, Julian Scheel <julian@jusst.de>
+ * Copyright (C) 2010 Google, Inc.
+ * Copyright (C) 2013 Avionic Design GmbH
  *
- *   This program is free software: you can redistribute it and/or modify
- *   it under the terms of the GNU General Public License as published by
- *   the Free Software Foundation, either version 3 of the License, or
- *   (at your option) any later version.
+ * This software is licensed under the terms of the GNU General Public
+ * License version 2, as published by the Free Software Foundation, and
+ * may be copied, distributed, and modified under those terms.
  *
- *   This program is distributed in the hope that it will be useful,
- *   but WITHOUT ANY WARRANTY; without even the implied warranty of
- *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *   GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *   You should have received a copy of the GNU General Public License
- *   along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef _MACH_TEGRA_BOARD_TEC_NG_H
