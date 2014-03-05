ecbfac8649f6758e24b12abb8c645af8140112af
Lars-Peter Clausen <lars@metafoo.de>
2014-03-05 13:17:44 +0100

    ASoC: Fix active count tracking for CODEC to CODEC links
    For CODEC to CODEC links we need to make sure to also manage the 'active' field
    of the cpu_dai CODEC.
    
    Change-Id: I57cee5d0ec590eebd90c792741aa65c8d931cb48
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Signed-off-by: Mark Brown <broonie@linaro.org>
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/3439
    Reviewed-by: Julian Scheel <julian@jusst.de>

--- sound/soc/soc-pcm.c
+++ sound/soc/soc-pcm.c
@@ -60,7 +60,9 @@ void snd_soc_runtime_activate(struct snd_soc_pcm_runtime *rtd, int stream)
 
 	cpu_dai->active++;
 	codec_dai->active++;
-	rtd->codec->active++;
+	if (cpu_dai->codec)
+		cpu_dai->codec->active++;
+	codec_dai->codec->active++;
 }
 
 /**
@@ -90,7 +92,9 @@ void snd_soc_runtime_deactivate(struct snd_soc_pcm_runtime *rtd, int stream)
 
 	cpu_dai->active--;
 	codec_dai->active--;
-	rtd->codec->active--;
+	if (cpu_dai->codec)
+		cpu_dai->codec->active--;
+	codec_dai->codec->active--;
 }
 
 /**
