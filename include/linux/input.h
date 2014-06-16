97b619beba329fd1d534569216c5d0cdf53e23d0
Alban Bedel <alban.bedel@avionic-design.de>
2014-06-16 16:31:16 +0200

    input: Add a switch to report the state of a phone hook
    Change-Id: I51e0e5bd9b4b41604df0ce9818996caf5f009c9f
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/1666
    Reviewed-by: Dirk Leber <dirk.leber@avionic-design.de>

--- include/linux/input.h
+++ include/linux/input.h
@@ -861,7 +861,8 @@ struct input_keymap_entry {
 #define SW_ROTATE_LOCK		0x0c  /* set = rotate locked/disabled */
 #define SW_LINEIN_INSERT	0x0d /* set = inserted */
 #define SW_MUTE_DEVICE		0x0e /* set = device disabled */
-#define SW_MAX			0x0f
+#define SW_PHONE_HOOK		0x0f /* set = phone off hook */
+#define SW_MAX			0x10
 #define SW_CNT			(SW_MAX+1)
 
 /*
