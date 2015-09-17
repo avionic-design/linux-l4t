c090c80860132adb38d2d16d9e50702d386bc637
Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
2015-09-17 10:41:29 +0200

    video: tegra: vi: Fix resource leaks in probe function
    While at it, switch to devm_regulator_get, and call it early to simplify
    probe deferral.
    
    Change-Id: If3911f65fb3f46c766ad887e81bdc5539e41e45a
    Signed-off-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>
    Reviewed-on: http://review.adnet.avionic-design.de/4186
    Reviewed-by: Alban Bedel <alban.bedel@avionic-design.de>

--- drivers/video/tegra/host/vi/vi.c
+++ drivers/video/tegra/host/vi/vi.c
@@ -129,6 +129,8 @@ static int vi_probe(struct platform_device *dev)
 	int err = 0;
 	struct vi *tegra_vi;
 	struct nvhost_device_data *pdata = NULL;
+	struct regulator *reg;
+
 	if (dev->dev.of_node) {
 		const struct of_device_id *match;
 
@@ -151,6 +153,18 @@ static int vi_probe(struct platform_device *dev)
 		return -ENODATA;
 	}
 
+	reg = devm_regulator_get(&dev->dev, "avdd_dsi_csi");
+	if (IS_ERR(reg)) {
+		err = PTR_ERR(reg);
+		if (err == -ENODEV)
+			dev_info(&dev->dev,
+				"%s: no regulator device\n", __func__);
+		else if (err != -EPROBE_DEFER)
+			dev_err(&dev->dev,
+				"%s: couldn't get regulator\n", __func__);
+		return err;
+	}
+
 	pdata->pdev = dev;
 	mutex_init(&pdata->lock);
 	platform_set_drvdata(dev, pdata);
@@ -167,6 +181,7 @@ static int vi_probe(struct platform_device *dev)
 	if (err)
 		goto vi_probe_fail;
 
+	tegra_vi->reg = reg;
 	tegra_vi->ndev = dev;
 
 	/* call vi_intr_init and stats_work */
@@ -185,25 +200,12 @@ static int vi_probe(struct platform_device *dev)
 	if (i2c_ctrl && i2c_ctrl->new_devices)
 		i2c_ctrl->new_devices(dev);
 
-	tegra_vi->reg = regulator_get(&tegra_vi->ndev->dev, "avdd_dsi_csi");
-	if (IS_ERR(tegra_vi->reg)) {
-		err = PTR_ERR(tegra_vi->reg);
-		if (err == -ENODEV)
-			dev_info(&tegra_vi->ndev->dev,
-				"%s: no regulator device\n", __func__);
-		else
-			dev_err(&tegra_vi->ndev->dev,
-				"%s: couldn't get regulator\n", __func__);
-		tegra_vi->reg = NULL;
-		goto camera_i2c_unregister;
-	}
-
 #ifdef CONFIG_TEGRA_CAMERA
 	tegra_vi->camera = tegra_camera_register(dev);
 	if (!tegra_vi->camera) {
 		dev_err(&dev->dev, "%s: can't register tegra_camera\n",
 				__func__);
-		goto vi_regulator_put;
+		goto camera_i2c_unregister;
 	}
 #endif
 
@@ -226,12 +228,8 @@ static int vi_probe(struct platform_device *dev)
 camera_unregister:
 #ifdef CONFIG_TEGRA_CAMERA
 	tegra_camera_unregister(tegra_vi->camera);
-vi_regulator_put:
-#endif
-	regulator_put(tegra_vi->reg);
-	tegra_vi->reg = NULL;
-
 camera_i2c_unregister:
+#endif
 	if (i2c_ctrl && i2c_ctrl->remove_devices)
 		i2c_ctrl->remove_devices(dev);
 	pdata->private_data = i2c_ctrl;
@@ -271,9 +269,6 @@ static int __exit vi_remove(struct platform_device *dev)
 #ifdef CONFIG_PM_GENERIC_DOMAINS
 	tegra_pd_remove_device(&dev->dev);
 #endif
-
-	regulator_put(tegra_vi->reg);
-	tegra_vi->reg = NULL;
 
 	/* Remove I2C Devices according to settings from board file */
 	if (i2c_ctrl && i2c_ctrl->remove_devices)
