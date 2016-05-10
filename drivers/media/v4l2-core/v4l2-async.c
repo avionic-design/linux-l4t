367ff48460edf7b4cba024dd6cc1c3812094f900
Alban Bedel <alban.bedel@avionic-design.de>
2016-05-10 11:59:11 +0200

    [media] v4l2-async: Pass the v4l2_async_subdev to the unbind callback
    v4l2_async_cleanup() is always called before calling the unbind()
    callback. However v4l2_async_cleanup() clears the asd member, so when
    calling the unbind() callback the v4l2_async_subdev is always NULL. To
    fix this save the asd before calling v4l2_async_cleanup().
    
    Change-Id: I4992a53616666f28dff91c22d2fc98ef89206fa5
    Signed-off-by: Alban Bedel <alban.bedel@avionic-design.de>
    Acked-by: Sakari Ailus <sakari.ailus@linux.intel.com>
    Reviewed-by: Javier Martinez Canillas <javier@osg.samsung.com>
    Reviewed-on: http://review.adnet.avionic-design.de/5579
    Reviewed-by: Nikolaus Schulz <nikolaus.schulz@avionic-design.de>

--- drivers/media/v4l2-core/v4l2-async.c
+++ drivers/media/v4l2-core/v4l2-async.c
@@ -202,6 +202,7 @@ void v4l2_async_notifier_unregister(struct v4l2_async_notifier *notifier)
 	list_del(&notifier->list);
 
 	list_for_each_entry_safe(sd, tmp, &notifier->done, async_list) {
+		struct v4l2_async_subdev *asd = sd->asd;
 		struct device *d;
 
 		d = get_device(sd->dev);
@@ -212,7 +213,7 @@ void v4l2_async_notifier_unregister(struct v4l2_async_notifier *notifier)
 		device_release_driver(d);
 
 		if (notifier->unbind)
-			notifier->unbind(notifier, sd, sd->asd);
+			notifier->unbind(notifier, sd, asd);
 
 		/*
 		 * Store device at the device cache, in order to call
@@ -287,6 +288,7 @@ EXPORT_SYMBOL(v4l2_async_register_subdev);
 void v4l2_async_unregister_subdev(struct v4l2_subdev *sd)
 {
 	struct v4l2_async_notifier *notifier = sd->notifier;
+	struct v4l2_async_subdev *asd = sd->asd;
 
 	if (!sd->asd) {
 		if (!list_empty(&sd->async_list))
@@ -301,7 +303,7 @@ void v4l2_async_unregister_subdev(struct v4l2_subdev *sd)
 	v4l2_async_cleanup(sd);
 
 	if (notifier->unbind)
-		notifier->unbind(notifier, sd, sd->asd);
+		notifier->unbind(notifier, sd, asd);
 
 	mutex_unlock(&list_lock);
 }
